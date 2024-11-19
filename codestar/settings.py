51c42c1c09f3d00f3ed467c30078d243965349a8
Zoltan Szarvas <szarvas.zoltan@hotmail.com>
2024-11-19 22:51:56 +0000

    test_forms.py files created and form tests writtenn

--- codestar/settings.py
+++ codestar/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 import os
+import sys
 import dj_database_url
 if os.path.isfile('env.py'):
     import env
@@ -108,6 +109,9 @@ WSGI_APPLICATION = 'codestar.wsgi.application'
 DATABASES = {
     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
+
+if 'test' in sys.argv:
+    DATABASES['default']['ENGINE'] = 'django.db.backends.sqlite3'
 
 CSRF_TRUSTED_ORIGINS = [
     "https://*.codeinstitute-ide.net/",
